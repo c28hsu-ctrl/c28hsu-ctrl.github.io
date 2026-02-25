1f8c9d2ca303988638bc9c4e5b451232825dc214
Chin-Hang Hsu <c28hsu@siena.edu>
2026-02-25 14:46:01 +0000

    Did slide 11, 14, 20

--- form/myform.js
+++ form/myform.js
@@ -11,3 +11,19 @@ buttonA.addEventListener('click', function() {
     alert(`Hello ${name}, nice to see you!`);
     headingA.textContent = `Welcome ${name}`;
 });
+
+
+const textbox= document.querySelector("#textBox");
+const output = document.querySelector("#output");
+output.textContent = "You typed:";
+textbox.addEventListener("keydown", (event) =>{
+    output.textContent = `${output.textContent} ${event.key}".`;
+});
+
+const button = document.querySelector("button");
+function greet(){
+    const name = promt("What is your name?");
+    const greeting = document.querySelector("#greeting");
+    greeting.textContent = `Hello! ${name}, nice to see you!`;
+}
+button.addEventListener("click", greet);
