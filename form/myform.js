7d4f0f25e6183f778cb38ccb8a77dcf64562bd45
Chin-Hang Hsu <c28hsu@siena.edu>
2026-02-25 14:04:25 +0000

    lab5 splash of js

---
+++ form/myform.js
@@ -0,0 +1,13 @@
+
+let a ="Hello";
+console.log("Hello World");
+alert("Nice day today");
+
+const buttonA = document.querySelector("#button_A");
+const headingA = document.getElementById("heading_A");
+
+buttonA.addEventListener('click', function() {
+    const name = prompt("What is your name?");
+    alert(`Hello ${name}, nice to see you!`);
+    headingA.textContent = `Welcome ${name}`;
+});
